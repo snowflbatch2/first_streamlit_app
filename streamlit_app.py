722a56d8998145ff6a5db2254ffd9ccd381c648d
snowflbatch2 <125303019+snowflbatch2@users.noreply.github.com>
2023-02-13 19:34:13 +0100

    Update streamlit_app.py
    breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
