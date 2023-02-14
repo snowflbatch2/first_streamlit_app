a1774f20e31f7f957c1154f4f5d095eb0ffdadf4
snowflbatch2 <125303019+snowflbatch2@users.noreply.github.com>
2023-02-14 11:24:28 +0100

    Update streamlit_app.py
    remove unnecessary display, give choice to select fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,10 +18,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #normalize json
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # display json
 streamlit.dataframe(fruityvice_normalized)
-streamlit.dataframe(fruityvice_response.json())
